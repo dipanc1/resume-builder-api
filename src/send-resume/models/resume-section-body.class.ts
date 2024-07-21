ad246336f6518ed8e093127ce3218ccc582b25a5
dipanc1 <dipanchhabra@gmail.com>
2024-07-21 15:26:09 +0530

    feat: Add IsString validation to prompt property in ResumeSectionBody class

--- src/send-resume/models/resume-section-body.class.ts
+++ src/send-resume/models/resume-section-body.class.ts
@@ -1,5 +1,7 @@
+import { IsString } from 'class-validator';
 import { ResumeBody } from './resume-body.class';
 
 export class ResumeSectionBody extends ResumeBody {
+  @IsString()
   prompt: string;
 }
