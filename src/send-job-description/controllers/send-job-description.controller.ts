87ab523bbdda6872217e5f9d5580f8e4b1fe994c
dipanc1 <dipanchhabra@gmail.com>
2024-08-05 21:31:10 +0530

    feat: Add endpoint for cleaning job description text
    Add a new endpoint in the SendJobDescriptionController to clean job description text. The endpoint is 'get-clean-job-description-text' and accepts a CleanJobDescriptionBody object in the request body. The job description text is cleaned by removing unnecessary characters, HTML tags, and special entities. This feature improves the accuracy and cleanliness of the extracted job description text.

--- src/send-job-description/controllers/send-job-description.controller.ts
+++ src/send-job-description/controllers/send-job-description.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
 
 import { Observable } from 'rxjs';
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { SendJobDescriptionService } from '../services/send-job-description.service';
 
 import { JobDescriptionBody } from '../models/job-description-body.class';
+import { CleanJobDescriptionBody } from '../models/clean-job-description-body.class';
 import { ResumeBody } from 'src/send-resume/models/resume-body.class';
 
 @Controller('send-job-description')
@@ -20,5 +21,15 @@ export class SendJobDescriptionController {
     @Body() jobDescription: JobDescriptionBody
   ): Observable<ResumeBody> {
     return this.sendJobDescriptionService.sendJobDescription(jobDescription);
+  }
+
+  @Post('get-clean-job-description-text')
+  @HttpCode(200)
+  getCleanJobDescriptionText(
+    @Body() jobDescription: CleanJobDescriptionBody
+  ): Observable<string> {
+    return this.sendJobDescriptionService.getCleanJobDescriptionText(
+      jobDescription
+    );
   }
 }
--- src/send-job-description/services/send-job-description.service.ts
+++ src/send-job-description/services/send-job-description.service.ts
@@ -1,6 +1,13 @@
 import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 
-import { Observable, catchError, firstValueFrom, from, switchMap } from 'rxjs';
+import {
+  Observable,
+  catchError,
+  firstValueFrom,
+  from,
+  of,
+  switchMap
+} from 'rxjs';
 
 import { AxiosError } from 'axios';
 
@@ -8,6 +15,10 @@ import { HttpService } from '@nestjs/axios';
 
 import { JobDescriptionBody } from '../models/job-description-body.class';
 import { ResumeBody } from 'src/send-resume/models/resume-body.class';
+import { CleanJobDescriptionBody } from '../models/clean-job-description-body.class';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cheerio = require('cheerio');
 
 @Injectable()
 export class SendJobDescriptionService {
@@ -40,5 +51,57 @@ export class SendJobDescriptionService {
           )
       )
     );
+  }
+
+  getCleanJobDescriptionText(
+    body: CleanJobDescriptionBody
+  ): Observable<string> {
+    const { jobDescription } = body;
+
+    const $ = cheerio.load(jobDescription, {
+      scriptingEnabled: false,
+      sourceCodeLocationInfo: false,
+      decodeEntities: false
+    });
+
+    const text = $.text()
+      .replace(/\s+/g, ' ')
+      .replace(/<script.*?<\/script>/g, '')
+      .replace(/<style.*?<\/style>/g, '')
+      .replace(/<.*?>/g, '')
+      .replace(/&nbsp;/g, ' ')
+      .replace(/&lt;/g, '<')
+      .replace(/&gt;/g, '>')
+      .replace(/&amp;/g, '&')
+      .replace(/&quot;/g, '"')
+      .replace(/&apos;/g, "'")
+      .replace(/&cent;/g, '¢')
+      .replace(/&pound;/g, '£')
+      .replace(/&yen;/g, '¥')
+      .replace(/&euro;/g, '€')
+      .replace(/&copy;/g, '©')
+      .replace(/&reg;/g, '®')
+      .replace(/&trade;/g, '™')
+      .replace(/&times;/g, '×')
+      .replace(/&divide;/g, '÷')
+      .replace(/&mdash;/g, '—')
+      .replace(/&ndash;/g, '–')
+      .replace(/&hellip;/g, '…')
+      .replace(/&laquo;/g, '«')
+      .replace(/&raquo;/g, '»')
+      .replace(/&ldquo;/g, '“')
+      .replace(/&rdquo;/g, '”')
+      .replace(/&lsquo;/g, '‘')
+      .replace(/&rsquo;/g, '’')
+      .replace(/&sbquo;/g, '‚')
+      .replace(/&bdquo;/g, '„')
+      .replace(/\[.*?\}\s/g, '')
+      .replace(/@.*?;/g, '')
+      .replace(/\{.*?\}/g, '')
+      .replace(/!function.*?\} } }/g, '')
+      .replace(/}/g, '')
+      .replace(/ ,/g, ',');
+
+    return of(text);
   }
 }
