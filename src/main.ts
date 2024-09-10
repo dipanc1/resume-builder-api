4e3c0771b78486407819f157313a01aface1fd23
dipanc1 <dipanchhabra@gmail.com>
2024-09-10 22:04:32 +0530

    Refactor status code handling in SaveResumeService and SendResumeController

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 
 import * as express from 'express';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
@@ -16,6 +19,11 @@ async function bootstrap() {
     origin: '*'
   });
   await app.listen(process.env.PORT || 3000);
+  fs.unlink('error.log', err => {
+    if (err) {
+      Logger.error(err);
+    }
+  });
   Logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
